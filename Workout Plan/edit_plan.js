a6473a36631d8be366a04f27db91c3b46afd12f7
Rajnesh28 <rajnesh.joshi28@gmail.com>
2022-05-03 14:59:44 -0700

    Adding plus icon beneath thursday

--- Workout Plan/edit_plan.js
+++ Workout Plan/edit_plan.js
@@ -1,8 +1,8 @@
-var d1 = document.getElementById('day-one-add')
+const d1 = document.getElementById('day-one-add')
 
 d1.addEventListener("click", addDay);
 
 function addDay(){
-    document.getElementById('day-one-add').style.zIndex = "-1";
+    d1.style.zIndex = "-1";
     document.getElementById('day1').style.zIndex = '0'
 }
